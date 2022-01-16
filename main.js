6d3425133b95625fb3452359a0742c2b9802afa4
Aswin <aswinkarat@gmail.com>
2022-01-16 20:17:28 +0530

    added commands for bot

--- main.js
+++ main.js
@@ -2,8 +2,23 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({intents: ['GUILD_MESSAGES', 'GUILDS']});
 
+const prefix = '-';
+
 client.once('ready', () => {
     console.log('Albega is online!');
 });
 
-client.login('OTMyMjc0MTU3MDc1NDM5NjY2.YeQl9w.6i7oqlNug1WL38whe0VHxE2WZPY');
+// To Add Logic to the User's Commands
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/_+/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping') {
+        message.channel.send('pong!');
+    }
+})
+
+// Token Key of the Bot
+client.login('ZTMyMjc0MTU3MDc1NDM5NjY2.YeQl9w.X2YYWbOvocSjNuXQpswNeEcx7PZ');
